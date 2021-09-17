942e77998248fd159387b266170bd22843c8d2f8
Sajevius <USER@USER-PC>
2021-09-17 11:57:21 +0800

    The beginning of everything and addition of Transcendeum dimension

---
+++ src/main/java/io/github/team_lodestar/transcendeum/item/TranscendeumItem.java
@@ -0,0 +1,45 @@
+
+package io.github.team_lodestar.transcendeum.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+
+import io.github.team_lodestar.transcendeum.world.dimension.TranscendeumDimension;
+
+public class TranscendeumItem extends Item {
+	@ObjectHolder("the_transcendeum:transcendeum")
+	public static final Item block = null;
+	public TranscendeumItem() {
+		super(new Item.Properties().group(ItemGroup.TOOLS).maxDamage(64));
+	}
+
+	@Override
+	public ActionResultType onItemUse(ItemUseContext context) {
+		PlayerEntity entity = context.getPlayer();
+		BlockPos pos = context.getPos().offset(context.getFace());
+		ItemStack itemstack = context.getItem();
+		World world = context.getWorld();
+		if (!entity.canPlayerEdit(pos, context.getFace(), itemstack)) {
+			return ActionResultType.FAIL;
+		} else {
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			boolean success = false;
+			if (world.isAirBlock(pos) && true) {
+				TranscendeumDimension.portal.portalSpawn(world, pos);
+				itemstack.damageItem(1, entity, c -> c.sendBreakAnimation(context.getHand()));
+				success = true;
+			}
+			return success ? ActionResultType.SUCCESS : ActionResultType.FAIL;
+		}
+	}
+}
