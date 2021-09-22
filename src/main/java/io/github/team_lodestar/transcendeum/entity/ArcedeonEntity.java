fbe180e482434432a17c488bd888b8cafcf3e761
AquexTheSeal <Shaun@Shadeyii>
2021-09-22 15:48:34 +0800

    added the arcedeon

---
+++ src/main/java/io/github/team_lodestar/transcendeum/entity/ArcedeonEntity.java
@@ -0,0 +1,290 @@
+
+package io.github.team_lodestar.transcendeum.entity;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.ForgeMod;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.pathfinding.SwimmerPathNavigator;
+import net.minecraft.network.IPacket;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.passive.SquidEntity;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.ai.goal.RandomSwimmingGoal;
+import net.minecraft.entity.ai.goal.OwnerHurtTargetGoal;
+import net.minecraft.entity.ai.goal.OwnerHurtByTargetGoal;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.FollowOwnerGoal;
+import net.minecraft.entity.ai.controller.MovementController;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.AgeableEntity;
+
+import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumMobsItemGroup;
+import io.github.team_lodestar.transcendeum.item.VirililyItem;
+import io.github.team_lodestar.transcendeum.entity.renderer.ArcedeonRenderer;
+import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
+
+@TheTranscendeumModElements.ModElement.Tag
+public class ArcedeonEntity extends TheTranscendeumModElements.ModElement {
+	public static EntityType entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.WATER_CREATURE)
+			.setShouldReceiveVelocityUpdates(true).setTrackingRange(64).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new)
+			.size(0.6f, 1.8f)).build("arcedeon").setRegistryName("arcedeon");
+	public ArcedeonEntity(TheTranscendeumModElements instance) {
+		super(instance, 158);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new ArcedeonRenderer.ModelRegisterHandler());
+		FMLJavaModLoadingContext.get().getModEventBus().register(new EntityAttributesRegisterHandler());
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> entity);
+		elements.items.add(() -> new SpawnEggItem(entity, -15724507, -6697729, new Item.Properties().group(TranscendeumMobsItemGroup.tab))
+				.setRegistryName("arcedeon_spawn_egg"));
+	}
+
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		boolean biomeCriteria = false;
+		if (new ResourceLocation("the_transcendeum:aurea_forest").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:aurea_plains").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:calid_woodlands").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:crystalia_forest").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:salt_lowlands").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:sullen_canyon").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:sullen_cliffs").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:sullen_desert").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:transcendent_ocean").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:transcendent_sea").equals(event.getName()))
+			biomeCriteria = true;
+		if (new ResourceLocation("the_transcendeum:viridian_mires").equals(event.getName()))
+			biomeCriteria = true;
+		if (!biomeCriteria)
+			return;
+		event.getSpawns().getSpawner(EntityClassification.WATER_CREATURE).add(new MobSpawnInfo.Spawners(entity, 20, 4, 4));
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		EntitySpawnPlacementRegistry.register(entity, EntitySpawnPlacementRegistry.PlacementType.IN_WATER, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES,
+				SquidEntity::func_223365_b);
+	}
+	private static class EntityAttributesRegisterHandler {
+		@SubscribeEvent
+		public void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+			AttributeModifierMap.MutableAttribute ammma = MobEntity.func_233666_p_();
+			ammma = ammma.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3);
+			ammma = ammma.createMutableAttribute(Attributes.MAX_HEALTH, 10);
+			ammma = ammma.createMutableAttribute(Attributes.ARMOR, 0);
+			ammma = ammma.createMutableAttribute(Attributes.ATTACK_DAMAGE, 3);
+			ammma = ammma.createMutableAttribute(ForgeMod.SWIM_SPEED.get(), 0.3);
+			event.put(entity, ammma.create());
+		}
+	}
+
+	public static class CustomEntity extends TameableEntity {
+		public CustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
+			this(entity, world);
+		}
+
+		public CustomEntity(EntityType<CustomEntity> type, World world) {
+			super(type, world);
+			experienceValue = 0;
+			setNoAI(false);
+			this.moveController = new MovementController(this) {
+				@Override
+				public void tick() {
+					if (this.action == MovementController.Action.MOVE_TO && !CustomEntity.this.getNavigator().noPath()) {
+						double dx = this.posX - CustomEntity.this.getPosX();
+						double dy = this.posY - CustomEntity.this.getPosY();
+						double dz = this.posZ - CustomEntity.this.getPosZ();
+						float f = (float) (MathHelper.atan2(dz, dx) * (double) (180 / Math.PI)) - 90;
+						CustomEntity.this.rotationYaw = this.limitAngle(CustomEntity.this.rotationYaw, f, 10);
+						CustomEntity.this.renderYawOffset = CustomEntity.this.rotationYaw;
+						CustomEntity.this.rotationYawHead = CustomEntity.this.rotationYaw;
+						float f1 = (float) (this.speed * CustomEntity.this.getAttribute(Attributes.MOVEMENT_SPEED).getValue());
+						if (CustomEntity.this.isInWater()) {
+							CustomEntity.this.setAIMoveSpeed(f1 * 0.1F);
+							float f2 = -(float) (MathHelper.atan2(dy, MathHelper.sqrt(dx * dx + dz * dz)) * (180F / Math.PI));
+							f2 = MathHelper.clamp(MathHelper.wrapDegrees(f2), -85, 85);
+							CustomEntity.this.rotationPitch = this.limitAngle(CustomEntity.this.rotationPitch, f2, 5);
+							float f3 = MathHelper.cos(CustomEntity.this.rotationPitch * (float) (Math.PI / 180.0));
+							float f4 = MathHelper.sin(CustomEntity.this.rotationPitch * (float) (Math.PI / 180.0));
+							CustomEntity.this.setMoveForward(f3 * f1);
+							CustomEntity.this.setMoveVertical((float) (f1 * dy));
+						} else {
+							CustomEntity.this.setAIMoveSpeed(f1 * 0.05F);
+						}
+					} else {
+						CustomEntity.this.setAIMoveSpeed(0);
+						CustomEntity.this.setMoveVertical(0);
+						CustomEntity.this.setMoveForward(0);
+					}
+				}
+			};
+			this.navigator = new SwimmerPathNavigator(this, this.world);
+		}
+
+		@Override
+		public IPacket<?> createSpawnPacket() {
+			return NetworkHooks.getEntitySpawningPacket(this);
+		}
+
+		@Override
+		protected void registerGoals() {
+			super.registerGoals();
+			this.goalSelector.addGoal(1, new RandomSwimmingGoal(this, 1, 40));
+			this.targetSelector.addGoal(2, new HurtByTargetGoal(this).setCallsForHelp(this.getClass()));
+			this.goalSelector.addGoal(3, new TemptGoal(this, 1, Ingredient.fromItems(VirililyItem.block), false));
+			this.goalSelector.addGoal(4, new FollowOwnerGoal(this, 1, (float) 10, (float) 2, false));
+			this.goalSelector.addGoal(5, new OwnerHurtTargetGoal(this));
+			this.goalSelector.addGoal(6, new OwnerHurtByTargetGoal(this));
+		}
+
+		@Override
+		public CreatureAttribute getCreatureAttribute() {
+			return CreatureAttribute.WATER;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.ambient"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.hurt"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.dolphin.death"));
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source == DamageSource.DROWN)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
+		public ActionResultType func_230254_b_(PlayerEntity sourceentity, Hand hand) {
+			ItemStack itemstack = sourceentity.getHeldItem(hand);
+			ActionResultType retval = ActionResultType.func_233537_a_(this.world.isRemote());
+			Item item = itemstack.getItem();
+			if (itemstack.getItem() instanceof SpawnEggItem) {
+				retval = super.func_230254_b_(sourceentity, hand);
+			} else if (this.world.isRemote()) {
+				retval = (this.isTamed() && this.isOwner(sourceentity) || this.isBreedingItem(itemstack))
+						? ActionResultType.func_233537_a_(this.world.isRemote())
+						: ActionResultType.PASS;
+			} else {
+				if (this.isTamed()) {
+					if (this.isOwner(sourceentity)) {
+						if (item.isFood() && this.isBreedingItem(itemstack) && this.getHealth() < this.getMaxHealth()) {
+							this.consumeItemFromStack(sourceentity, itemstack);
+							this.heal((float) item.getFood().getHealing());
+							retval = ActionResultType.func_233537_a_(this.world.isRemote());
+						} else if (this.isBreedingItem(itemstack) && this.getHealth() < this.getMaxHealth()) {
+							this.consumeItemFromStack(sourceentity, itemstack);
+							this.heal(4);
+							retval = ActionResultType.func_233537_a_(this.world.isRemote());
+						} else {
+							retval = super.func_230254_b_(sourceentity, hand);
+						}
+					}
+				} else if (this.isBreedingItem(itemstack)) {
+					this.consumeItemFromStack(sourceentity, itemstack);
+					if (this.rand.nextInt(3) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, sourceentity)) {
+						this.setTamedBy(sourceentity);
+						this.world.setEntityState(this, (byte) 7);
+					} else {
+						this.world.setEntityState(this, (byte) 6);
+					}
+					this.enablePersistence();
+					retval = ActionResultType.func_233537_a_(this.world.isRemote());
+				} else {
+					retval = super.func_230254_b_(sourceentity, hand);
+					if (retval == ActionResultType.SUCCESS || retval == ActionResultType.CONSUME)
+						this.enablePersistence();
+				}
+			}
+			return retval;
+		}
+
+		@Override
+		public AgeableEntity func_241840_a(ServerWorld serverWorld, AgeableEntity ageable) {
+			CustomEntity retval = (CustomEntity) entity.create(serverWorld);
+			retval.onInitialSpawn(serverWorld, serverWorld.getDifficultyForLocation(new BlockPos(retval.getPosition())), SpawnReason.BREEDING,
+					(ILivingEntityData) null, (CompoundNBT) null);
+			return retval;
+		}
+
+		@Override
+		public boolean isBreedingItem(ItemStack stack) {
+			if (stack == null)
+				return false;
+			if (VirililyItem.block == stack.getItem())
+				return true;
+			return false;
+		}
+
+		@Override
+		public boolean canBreatheUnderwater() {
+			return true;
+		}
+
+		@Override
+		public boolean isNotColliding(IWorldReader worldreader) {
+			return worldreader.checkNoEntityCollision(this, VoxelShapes.create(this.getBoundingBox()));
+		}
+
+		@Override
+		public boolean isPushedByWater() {
+			return false;
+		}
+	}
+}
