8133786826f4a2e4708ba9edd43eab9d57703ad9
Sajevius <USER@USER-PC>
2021-09-24 10:52:01 +0800

    Fixed build errors w/ Morosite blocks

--- src/main/java/io/github/team_lodestar/transcendeum/block/PolishedMorositePressurePlateBlock.java
+++ src/main/java/io/github/team_lodestar/transcendeum/block/PolishedMorositePressurePlateBlock.java
@@ -1,18 +1,33 @@
 
 package io.github.team_lodestar.transcendeum.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.PressurePlateBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumBlocksItemGroup;
+import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
 
 @TheTranscendeumModElements.ModElement.Tag
 public class PolishedMorositePressurePlateBlock extends TheTranscendeumModElements.ModElement {
-
 	@ObjectHolder("the_transcendeum:polished_morosite_pressure_plate")
 	public static final Block block = null;
-
 	public PolishedMorositePressurePlateBlock(TheTranscendeumModElements instance) {
 		super(instance, 176);
-
 	}
 
 	@Override
@@ -21,13 +36,10 @@ public class PolishedMorositePressurePlateBlock extends TheTranscendeumModElemen
 		elements.items.add(
 				() -> new BlockItem(block, new Item.Properties().group(TranscendeumBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends PressurePlateBlock {
-
 		public CustomBlock() {
 			super(Sensitivity.MOBS, Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(0.9f, 0.9f)
 					.setLightLevel(s -> 0).harvestLevel(0).harvestTool(ToolType.PICKAXE).setRequiresTool().tickRandomly());
-
 			setRegistryName("polished_morosite_pressure_plate");
 		}
 
@@ -38,13 +50,10 @@ public class PolishedMorositePressurePlateBlock extends TheTranscendeumModElemen
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
--- src/main/java/io/github/team_lodestar/transcendeum/item/BerthelArmorItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/BerthelArmorItem.java
@@ -1,21 +1,33 @@
 
 package io.github.team_lodestar.transcendeum.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
+
 @TheTranscendeumModElements.ModElement.Tag
 public class BerthelArmorItem extends TheTranscendeumModElements.ModElement {
-
 	@ObjectHolder("the_transcendeum:berthel_armor_helmet")
 	public static final Item helmet = null;
-
 	@ObjectHolder("the_transcendeum:berthel_armor_chestplate")
 	public static final Item body = null;
-
 	@ObjectHolder("the_transcendeum:berthel_armor_leggings")
 	public static final Item legs = null;
-
 	@ObjectHolder("the_transcendeum:berthel_armor_boots")
 	public static final Item boots = null;
-
 	public BerthelArmorItem(TheTranscendeumModElements instance) {
 		super(instance, 169);
 	}
@@ -64,42 +76,29 @@ public class BerthelArmorItem extends TheTranscendeumModElements.ModElement {
 				return 0f;
 			}
 		};
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("berthel_armor_helmet"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("berthel_armor_chestplate"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("berthel_armor_leggings"));
-
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
-
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-
 		}.setRegistryName("berthel_armor_boots"));
 	}
-
 }
