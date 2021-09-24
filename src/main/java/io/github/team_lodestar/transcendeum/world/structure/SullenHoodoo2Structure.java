78693217d0ab93ff592e8d9c2f477a7e6901c974
Sajevius <USER@USER-PC>
2021-09-24 20:11:12 +0800

    Biome parenting tweaks + structure gen tweaks + new mod info + new logo!

--- src/main/java/io/github/team_lodestar/transcendeum/world/structure/SullenHoodoo2Structure.java
+++ src/main/java/io/github/team_lodestar/transcendeum/world/structure/SullenHoodoo2Structure.java
@@ -50,7 +50,7 @@ public class SullenHoodoo2Structure {
 					if (!dimensionCriteria)
 						return false;
 					if ((random.nextInt(1000000) + 1) <= 800000) {
-						int count = random.nextInt(1) + 1;
+						int count = random.nextInt(4) + 1;
 						for (int a = 0; a < count; a++) {
 							int i = ci + random.nextInt(16);
 							int k = ck + random.nextInt(16);
