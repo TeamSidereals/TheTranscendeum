9afdfffcfffafd91be2ce8050909eaf52fc7f029
Sajevius <USER@USER-PC>
2021-09-28 12:14:54 +0800

    Added special AoE damage ability to cadillacium tools while attacking in Transcendeum

--- src/main/java/io/github/team_lodestar/transcendeum/item/CadillaciumSwordItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/CadillaciumSwordItem.java
@@ -3,12 +3,18 @@ package io.github.team_lodestar.transcendeum.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
+import net.minecraft.entity.LivingEntity;
 
+import java.util.Map;
+import java.util.HashMap;
+
+import io.github.team_lodestar.transcendeum.procedures.CadillaciumToolsHitLivingEntityProcedure;
 import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumGearItemGroup;
 import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
 
@@ -47,6 +53,25 @@ public class CadillaciumSwordItem extends TheTranscendeumModElements.ModElement 
 				return Ingredient.fromStacks(new ItemStack(CadillaciumIngotItem.block));
 			}
 		}, 3, -2.4f, new Item.Properties().group(TranscendeumGearItemGroup.tab)) {
+			@Override
+			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				World world = entity.world;
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					$_dependencies.put("sourceentity", sourceentity);
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					CadillaciumToolsHitLivingEntityProcedure.executeProcedure($_dependencies);
+				}
+				return retval;
+			}
 		}.setRegistryName("cadillacium_sword"));
 	}
 }
