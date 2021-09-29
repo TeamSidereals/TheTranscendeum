eee2a54bbc61ee293ff25175f99747841091e78e
AquexTheSeal <Shaun@Shadeyii>
2021-09-29 17:23:05 +0800

    folder reorganizations and error fixes :why:

--- src/main/java/io/github/team_lodestar/transcendeum/block/ViridwoodButtonBlock.java
+++ src/main/java/io/github/team_lodestar/transcendeum/block/ViridwoodButtonBlock.java
@@ -1,18 +1,33 @@
 
 package io.github.team_lodestar.transcendeum.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.block.WoodButtonBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumBlocksItemGroup;
+import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
 
 @TheTranscendeumModElements.ModElement.Tag
 public class ViridwoodButtonBlock extends TheTranscendeumModElements.ModElement {
-
 	@ObjectHolder("the_transcendeum:viridwood_button")
 	public static final Block block = null;
-
 	public ViridwoodButtonBlock(TheTranscendeumModElements instance) {
 		super(instance, 233);
-
 	}
 
 	@Override
@@ -21,12 +36,9 @@ public class ViridwoodButtonBlock extends TheTranscendeumModElements.ModElement 
 		elements.items.add(
 				() -> new BlockItem(block, new Item.Properties().group(TranscendeumBlocksItemGroup.tab)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends WoodButtonBlock {
-
 		public CustomBlock() {
 			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 3f).setLightLevel(s -> 0));
-
 			setRegistryName("viridwood_button");
 		}
 
@@ -37,13 +49,10 @@ public class ViridwoodButtonBlock extends TheTranscendeumModElements.ModElement 
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
