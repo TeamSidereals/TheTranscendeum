c5b205fe09a92069041f45d1eb3b988f2cd4f2e7
AquexTheSeal <Shaun@Shadeyii>
2021-10-03 19:50:48 +0800

    more chrysalium vine tweaks

--- src/main/java/io/github/team_lodestar/transcendeum/block/ChrysaliumVineBlock.java
+++ src/main/java/io/github/team_lodestar/transcendeum/block/ChrysaliumVineBlock.java
@@ -136,7 +136,7 @@ public class ChrysaliumVineBlock extends TheTranscendeumModElements.ModElement {
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
-			return Collections.singletonList(new ItemStack(this, 1));
+			return Collections.singletonList(new ItemStack(ChrysaliumVineBottomBlock.block));
 		}
 
 		@Override
