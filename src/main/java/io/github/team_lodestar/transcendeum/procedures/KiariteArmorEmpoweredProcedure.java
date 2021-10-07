61f14fde206f88927d6e4bd8cdf6c7529cb8a3d3
AquexTheSeal <Shaun@Shadeyii>
2021-10-07 20:28:33 +0800

    Updated armor abilities

--- src/main/java/io/github/team_lodestar/transcendeum/procedures/KiariteArmorEmpoweredProcedure.java
+++ src/main/java/io/github/team_lodestar/transcendeum/procedures/KiariteArmorEmpoweredProcedure.java
@@ -1,7 +1,31 @@
 package io.github.team_lodestar.transcendeum.procedures;
 
-public class KiariteArmorEmpoweredProcedure {
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tags.EntityTypeTags;
+import net.minecraft.item.ItemStack;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import io.github.team_lodestar.transcendeum.particle.KiariteExorcismParticle;
+import io.github.team_lodestar.transcendeum.item.KiariteArmorItem;
+import io.github.team_lodestar.transcendeum.TheTranscendeumMod;
+
+public class KiariteArmorEmpoweredProcedure {
 	@Mod.EventBusSubscriber
 	private static class GlobalTrigger {
 		@SubscribeEvent
@@ -29,7 +53,6 @@ public class KiariteArmorEmpoweredProcedure {
 			}
 		}
 	}
-
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
@@ -61,33 +84,33 @@ public class KiariteArmorEmpoweredProcedure {
 				TheTranscendeumMod.LOGGER.warn("Failed to load dependency world for procedure KiariteArmorEmpowered!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
 		Entity sourceentity = (Entity) dependencies.get("sourceentity");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-
 		if ((EntityTypeTags.getCollection().getTagByID(new ResourceLocation(("forge:transcendent_mobs").toLowerCase(java.util.Locale.ENGLISH)))
 				.contains(sourceentity.getType()))) {
 			if ((((entity instanceof LivingEntity)
 					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 3))
 					: ItemStack.EMPTY).getItem() == KiariteArmorItem.helmet)) {
 				if ((Math.random() < 0.05)) {
-					entity.setMotion((entity.getMotion().getX()), ((entity.getMotion().getY()) + 0.55), (entity.getMotion().getZ()));
+					sourceentity.setMotion((sourceentity.getMotion().getX()), 0.55, (sourceentity.getMotion().getZ()));
 					if (world instanceof ServerWorld) {
 						((ServerWorld) world).spawnParticle(KiariteExorcismParticle.particle, (entity.getPosX()), ((entity.getPosY()) + 1),
 								(entity.getPosZ()), (int) 10, 0.2, 0.4, 0.2, 0.1);
 					}
 					if (world instanceof World && !world.isRemote()) {
 						((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1);
 					} else {
 						((World) world).playSound(x, y, z,
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1, false);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1, false);
 					}
 					if (dependencies.get("event") != null) {
 						Object _obj = dependencies.get("event");
@@ -103,19 +126,21 @@ public class KiariteArmorEmpoweredProcedure {
 					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 2))
 					: ItemStack.EMPTY).getItem() == KiariteArmorItem.body)) {
 				if ((Math.random() < 0.05)) {
-					entity.setMotion((entity.getMotion().getX()), ((entity.getMotion().getY()) + 0.55), (entity.getMotion().getZ()));
+					sourceentity.setMotion((sourceentity.getMotion().getX()), 0.55, (sourceentity.getMotion().getZ()));
 					if (world instanceof ServerWorld) {
 						((ServerWorld) world).spawnParticle(KiariteExorcismParticle.particle, (entity.getPosX()), ((entity.getPosY()) + 1),
 								(entity.getPosZ()), (int) 10, 0.2, 0.4, 0.2, 0.1);
 					}
 					if (world instanceof World && !world.isRemote()) {
 						((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1);
 					} else {
 						((World) world).playSound(x, y, z,
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1, false);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1, false);
 					}
 					if (dependencies.get("event") != null) {
 						Object _obj = dependencies.get("event");
@@ -131,19 +156,21 @@ public class KiariteArmorEmpoweredProcedure {
 					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 1))
 					: ItemStack.EMPTY).getItem() == KiariteArmorItem.legs)) {
 				if ((Math.random() < 0.05)) {
-					entity.setMotion((entity.getMotion().getX()), ((entity.getMotion().getY()) + 0.55), (entity.getMotion().getZ()));
+					sourceentity.setMotion((sourceentity.getMotion().getX()), 0.55, (sourceentity.getMotion().getZ()));
 					if (world instanceof ServerWorld) {
 						((ServerWorld) world).spawnParticle(KiariteExorcismParticle.particle, (entity.getPosX()), ((entity.getPosY()) + 1),
 								(entity.getPosZ()), (int) 10, 0.2, 0.4, 0.2, 0.1);
 					}
 					if (world instanceof World && !world.isRemote()) {
 						((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1);
 					} else {
 						((World) world).playSound(x, y, z,
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1, false);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1, false);
 					}
 					if (dependencies.get("event") != null) {
 						Object _obj = dependencies.get("event");
@@ -159,19 +186,21 @@ public class KiariteArmorEmpoweredProcedure {
 					? ((LivingEntity) entity).getItemStackFromSlot(EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, (int) 0))
 					: ItemStack.EMPTY).getItem() == KiariteArmorItem.boots)) {
 				if ((Math.random() < 0.05)) {
-					entity.setMotion((entity.getMotion().getX()), ((entity.getMotion().getY()) + 0.55), (entity.getMotion().getZ()));
+					sourceentity.setMotion((sourceentity.getMotion().getX()), 0.55, (sourceentity.getMotion().getZ()));
 					if (world instanceof ServerWorld) {
 						((ServerWorld) world).spawnParticle(KiariteExorcismParticle.particle, (entity.getPosX()), ((entity.getPosY()) + 1),
 								(entity.getPosZ()), (int) 10, 0.2, 0.4, 0.2, 0.1);
 					}
 					if (world instanceof World && !world.isRemote()) {
 						((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1);
 					} else {
 						((World) world).playSound(x, y, z,
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.anvil.fall")),
-								SoundCategory.NEUTRAL, (float) 0.02, (float) 1, false);
+								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
+										.getValue(new ResourceLocation("entity.elder_guardian.curse")),
+								SoundCategory.NEUTRAL, (float) 0.5, (float) 1, false);
 					}
 					if (dependencies.get("event") != null) {
 						Object _obj = dependencies.get("event");
@@ -185,5 +214,4 @@ public class KiariteArmorEmpoweredProcedure {
 			}
 		}
 	}
-
 }
