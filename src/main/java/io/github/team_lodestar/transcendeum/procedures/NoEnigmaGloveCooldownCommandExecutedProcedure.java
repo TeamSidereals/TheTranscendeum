18d5baffa2f809c1549db7a7120deaea429a6100
Toma400 <Balmora@DESKTOP-RP03LP2>
2021-10-07 13:00:37 +0200

    Again stupid MCreator bug

--- src/main/java/io/github/team_lodestar/transcendeum/procedures/NoEnigmaGloveCooldownCommandExecutedProcedure.java
+++ src/main/java/io/github/team_lodestar/transcendeum/procedures/NoEnigmaGloveCooldownCommandExecutedProcedure.java
@@ -1,16 +1,22 @@
 package io.github.team_lodestar.transcendeum.procedures;
 
-public class NoEnigmaGloveCooldownCommandExecutedProcedure {
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
 
+import java.util.Map;
+
+import io.github.team_lodestar.transcendeum.TheTranscendeumModVariables;
+import io.github.team_lodestar.transcendeum.TheTranscendeumMod;
+
+public class NoEnigmaGloveCooldownCommandExecutedProcedure {
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
 				TheTranscendeumMod.LOGGER.warn("Failed to load dependency entity for procedure NoEnigmaGloveCooldownCommandExecuted!");
 			return;
 		}
-
 		Entity entity = (Entity) dependencies.get("entity");
-
 		if ((TheTranscendeumModVariables.NoEnigmaCooldown == (false))) {
 			TheTranscendeumModVariables.NoEnigmaCooldown = (boolean) (true);
 			if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
@@ -23,5 +29,4 @@ public class NoEnigmaGloveCooldownCommandExecutedProcedure {
 			}
 		}
 	}
-
 }
