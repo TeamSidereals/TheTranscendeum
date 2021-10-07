30819d3b38f435932627f41c833406a470375e67
AquexTheSeal <Shaun@Shadeyii>
2021-10-07 16:24:52 +0800

    added the Kiarite armor. Now gave the armors an equipment sound effect.

--- src/main/java/io/github/team_lodestar/transcendeum/item/KiariteArmorItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/KiariteArmorItem.java
@@ -19,17 +19,17 @@ import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumGearItemGroup;
 import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
 
 @TheTranscendeumModElements.ModElement.Tag
-public class BerthelArmorItem extends TheTranscendeumModElements.ModElement {
-	@ObjectHolder("the_transcendeum:berthel_armor_helmet")
+public class KiariteArmorItem extends TheTranscendeumModElements.ModElement {
+	@ObjectHolder("the_transcendeum:kiarite_armor_helmet")
 	public static final Item helmet = null;
-	@ObjectHolder("the_transcendeum:berthel_armor_chestplate")
+	@ObjectHolder("the_transcendeum:kiarite_armor_chestplate")
 	public static final Item body = null;
-	@ObjectHolder("the_transcendeum:berthel_armor_leggings")
+	@ObjectHolder("the_transcendeum:kiarite_armor_leggings")
 	public static final Item legs = null;
-	@ObjectHolder("the_transcendeum:berthel_armor_boots")
+	@ObjectHolder("the_transcendeum:kiarite_armor_boots")
 	public static final Item boots = null;
-	public BerthelArmorItem(TheTranscendeumModElements instance) {
-		super(instance, 169);
+	public KiariteArmorItem(TheTranscendeumModElements instance) {
+		super(instance, 334);
 	}
 
 	@Override
@@ -37,68 +37,68 @@ public class BerthelArmorItem extends TheTranscendeumModElements.ModElement {
 		IArmorMaterial armormaterial = new IArmorMaterial() {
 			@Override
 			public int getDurability(EquipmentSlotType slot) {
-				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 22;
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 26;
 			}
 
 			@Override
 			public int getDamageReductionAmount(EquipmentSlotType slot) {
-				return new int[]{2, 6, 7, 3}[slot.getIndex()];
+				return new int[]{4, 7, 9, 4}[slot.getIndex()];
 			}
 
 			@Override
 			public int getEnchantability() {
-				return 12;
+				return 15;
 			}
 
 			@Override
 			public net.minecraft.util.SoundEvent getSoundEvent() {
-				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.armor.equip_diamond"));
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("item.armor.equip_netherite"));
 			}
 
 			@Override
 			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack(BerthelShardItem.block));
+				return Ingredient.fromStacks(new ItemStack(KiariteIngotItem.block));
 			}
 
 			@OnlyIn(Dist.CLIENT)
 			@Override
 			public String getName() {
-				return "berthel_armor";
+				return "kiarite_armor";
 			}
 
 			@Override
 			public float getToughness() {
-				return 0.5f;
+				return 0f;
 			}
 
 			@Override
 			public float getKnockbackResistance() {
-				return 0f;
+				return 0.1f;
 			}
 		};
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(TranscendeumGearItemGroup.tab)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "the_transcendeum:textures/models/armor/kiarite__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("berthel_armor_helmet"));
+		}.setRegistryName("kiarite_armor_helmet"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(TranscendeumGearItemGroup.tab)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "the_transcendeum:textures/models/armor/kiarite__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("berthel_armor_chestplate"));
+		}.setRegistryName("kiarite_armor_chestplate"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(TranscendeumGearItemGroup.tab)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "the_transcendeum:textures/models/armor/kiarite__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("berthel_armor_leggings"));
+		}.setRegistryName("kiarite_armor_leggings"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(TranscendeumGearItemGroup.tab)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "the_transcendeum:textures/models/armor/berthel__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "the_transcendeum:textures/models/armor/kiarite__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("berthel_armor_boots"));
+		}.setRegistryName("kiarite_armor_boots"));
 	}
 }
