1e57d8d9f72ab6554991b83c881bcb0dee82e1d2
AquexTheSeal <Shaun@Shadeyii>
2021-10-16 13:53:40 +0800

    Added cool and swag descriptions for the Kiarite Gear!

--- src/main/java/io/github/team_lodestar/transcendeum/item/KiariteSwordItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/KiariteSwordItem.java
@@ -4,14 +4,18 @@ package io.github.team_lodestar.transcendeum.item;
 import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.util.ITooltipFlag;
 
 import java.util.Map;
+import java.util.List;
 import java.util.HashMap;
 
 import io.github.team_lodestar.transcendeum.procedures.KiariteToolLivingEntityIsHitWithToolProcedure;
@@ -54,6 +58,14 @@ public class KiariteSwordItem extends TheTranscendeumModElements.ModElement {
 			}
 		}, 3, -2.4f, new Item.Properties().group(TranscendeumGearItemGroup.tab).isImmuneToFire()) {
 			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("\u00A78A tool empowered by the ancient dragons."));
+				list.add(new StringTextComponent("\u00A76Knocks up and deals extra 4 damage to the target"));
+				list.add(new StringTextComponent("only if the target is a mob of the Transcendeum dimension."));
+			}
+
+			@Override
 			public boolean hitEntity(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
 				boolean retval = super.hitEntity(itemstack, entity, sourceentity);
 				double x = entity.getPosX();
@@ -63,6 +75,7 @@ public class KiariteSwordItem extends TheTranscendeumModElements.ModElement {
 				{
 					Map<String, Object> $_dependencies = new HashMap<>();
 					$_dependencies.put("entity", entity);
+					$_dependencies.put("itemstack", itemstack);
 					$_dependencies.put("world", world);
 					KiariteToolLivingEntityIsHitWithToolProcedure.executeProcedure($_dependencies);
 				}
