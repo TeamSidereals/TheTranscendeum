02b193da7a47b2387bd3566e41b7cb0c826ed8ae
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-02-01 22:47:41 +0700

    add Staff of Biosphere, a staff that have different ability base on biome player in (currently has nothing)

--- src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/MixCodeItemRegister.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/MixCodeItemRegister.java
@@ -1,6 +1,7 @@
 package io.github.team_lodestar.transcendeum.item.mixcode;
 
 import io.github.team_lodestar.transcendeum.TheTranscendeumMod;
+import io.github.team_lodestar.transcendeum.item.mixcode.custom.StaffOfBiosphereItem;
 import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumItemsItemGroup;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,6 +17,11 @@ public class MixCodeItemRegister {
 
     public static final RegistryObject<Item> MEDAL = ITEMS.register("medal", () ->
             new Item(
+                    new Item.Properties().group(TranscendeumItemsItemGroup.tab)
+            )
+    );
+    public static final RegistryObject<Item> STAFF_OF_BIOSPHERE = ITEMS.register("staff_of_biosphere", () ->
+            new StaffOfBiosphereItem(
                     new Item.Properties().group(TranscendeumItemsItemGroup.tab)
             )
     );
