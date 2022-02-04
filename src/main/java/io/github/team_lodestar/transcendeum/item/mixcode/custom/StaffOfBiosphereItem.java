da9f8b8ab647de226ac125324711be8806c27bdf
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-02-04 13:35:50 +0700

    delete Greed Scepter

--- src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/StaffOfBiosphereItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/StaffOfBiosphereItem.java
@@ -1,5 +1,8 @@
 package io.github.team_lodestar.transcendeum.item.mixcode.custom;
 
+import io.github.team_lodestar.transcendeum.item.EnigmaGloveItem;
+import io.github.team_lodestar.transcendeum.world.biome.TranscendentOceanBiome;
+import io.github.team_lodestar.transcendeum.world.biome.TranscendentSeaBiome;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -20,8 +23,23 @@ public class StaffOfBiosphereItem extends Item {
     }
 
     @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand handIn) {
+    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
+        player.setActiveHand(hand);
+        return ActionResult.resultPass(player.getHeldItem(hand));
+    }
+
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World world, LivingEntity entity, int timeLeft) {
         System.out.println("you just used the staff");
-        return super.onItemRightClick(world, player, handIn);
+        if (entity instanceof PlayerEntity) {
+            if (world.getBiome(entity.getPosition()).getRegistryName().equals(TranscendentOceanBiome.biome.getRegistryName())
+            || world.getBiome(entity.getPosition()).getRegistryName().equals(TranscendentSeaBiome.biome.getRegistryName())){
+                System.out.println("in ocean/sea biome");
+            }
+            if (!((PlayerEntity) entity).isCreative() && !entity.isSpectator()) {
+                ((PlayerEntity) entity).getCooldownTracker().setCooldown(this, (int) 200);
+                System.out.println("on cooldown");
+            }
+        }
     }
 }
