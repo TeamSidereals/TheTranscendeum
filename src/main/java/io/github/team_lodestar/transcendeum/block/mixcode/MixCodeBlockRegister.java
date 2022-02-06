5718a7047701fad91b7f19ec13374695fda4399f
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-02-06 14:50:49 +0700

    add Viriclinger Rope

---
+++ src/main/java/io/github/team_lodestar/transcendeum/block/mixcode/MixCodeBlockRegister.java
@@ -0,0 +1,40 @@
+package io.github.team_lodestar.transcendeum.block.mixcode;
+
+import io.github.team_lodestar.transcendeum.TheTranscendeumMod;
+import io.github.team_lodestar.transcendeum.item.mixcode.MixCodeItemRegister;
+import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumItemsItemGroup;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class MixCodeBlockRegister {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheTranscendeumMod.MODID);
+    public static void init(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
+        RegistryObject<T> returnBlock = BLOCKS.register(name, block);
+        registerBlockItem(name, returnBlock);
+        return returnBlock;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
+        MixCodeItemRegister.ITEMS.register(name, () ->
+                new BlockItem(block.get(),
+                        new Item.Properties().group(TranscendeumItemsItemGroup.tab)));
+    }
+
+    public static final RegistryObject<Block> VIRICLINGER_ROPE = BLOCKS.register("viriclinger_rope", () ->
+            new ViriclingerRopeBlock(AbstractBlock.Properties.create(Material.MISCELLANEOUS)
+                    .doesNotBlockMovement()
+                    .hardnessAndResistance(0,0)
+            )
+    );
+}
---
+++ src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/ViriclingerBlockItem.java
@@ -0,0 +1,41 @@
+package io.github.team_lodestar.transcendeum.item.mixcode.custom;
+
+import io.github.team_lodestar.transcendeum.block.mixcode.MixCodeBlockRegister;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.*;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+
+public class ViriclingerBlockItem extends BlockItem {
+
+    public ViriclingerBlockItem(Block block, Properties builder) {
+        super(block, builder);
+    }
+
+    @Nullable
+    public BlockItemUseContext getBlockItemUseContext(BlockItemUseContext context) {
+        BlockPos blockpos = context.getPos();
+        World world = context.getWorld();
+        BlockState blockstate = world.getBlockState(blockpos);
+        Block block = this.getBlock();
+        Direction direction= Direction.DOWN;
+        if (!blockstate.isIn(block)) {
+            return context;
+        } else {
+            BlockPos.Mutable blockpos$mutable = blockpos.toMutable().move(direction);
+            while(world.getBlockState(blockpos$mutable).getBlock() == MixCodeBlockRegister.VIRICLINGER_ROPE.get()) {
+                blockpos$mutable.move(direction);
+            }
+            if (blockstate.isReplaceable(context)) {
+                return BlockItemUseContext.func_221536_a(context, blockpos$mutable, direction);
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
