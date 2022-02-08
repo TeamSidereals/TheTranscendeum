698045d232c0157f031eb8ecfede8dcc2244b80f
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-02-08 15:18:02 +0700

    add purify powder (no function)

--- src/main/java/io/github/team_lodestar/transcendeum/entity/mixcode/custom/PurifyPowderEntity.java
+++ src/main/java/io/github/team_lodestar/transcendeum/entity/mixcode/custom/PurifyPowderEntity.java
@@ -1,31 +1,20 @@
 package io.github.team_lodestar.transcendeum.entity.mixcode.custom;
 
 import io.github.team_lodestar.transcendeum.entity.mixcode.MixCodeEntityType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.*;
-import net.minecraft.entity.monster.ZombieVillagerEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import io.github.team_lodestar.transcendeum.item.mixcode.MixCodeItemRegister;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.network.IPacket;
-import net.minecraft.particles.ParticleTypes;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
-import java.util.Random;
-
 public class PurifyPowderEntity extends ProjectileItemEntity {
 
     public PurifyPowderEntity(EntityType<? extends PurifyPowderEntity> type, World world) {
-        super(type,world);
+        super(type, world);
     }
 
     public PurifyPowderEntity(World worldIn, LivingEntity entity) {
@@ -38,7 +27,7 @@ public class PurifyPowderEntity extends ProjectileItemEntity {
 
     @Override
     protected Item getDefaultItem() {
-        return null;
+        return MixCodeItemRegister.PURIFY_POWDER.get();
     }
 
     @Override
@@ -48,44 +37,8 @@ public class PurifyPowderEntity extends ProjectileItemEntity {
 
     @Override
     protected void onImpact(RayTraceResult result) {
-        if (!world.isRemote){
-            if (result.getType() == RayTraceResult.Type.ENTITY){
-                Entity entity = ((EntityRayTraceResult) result).getEntity();
-                if (entity instanceof MobEntity){
-                    CreatureAttribute attribute = ((MobEntity) entity).getCreatureAttribute();
-                    if (attribute == CreatureAttribute.UNDEAD){
-                        entity.attackEntityFrom(DamageSource.MAGIC, 2);
-                        ((MobEntity) entity).addPotionEffect(new EffectInstance(Effects.WEAKNESS, 100, 0));
-                        ((MobEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, 100, 0));
-                    }
-                    if (entity instanceof ZombieVillagerEntity){
-                        Random roll = new Random();
-                        if (roll.nextDouble() < 0.1D) {
-                            PlayerEntity player = null;
-                            if (this.func_234616_v_() instanceof PlayerEntity){
-                                player = (PlayerEntity) this.func_234616_v_();
-                            }
-                            ((ZombieVillagerEntity) entity).startConverting(player.getUniqueID(), roll.nextInt(2401) + 3600);
-                        }
-                    }
-                }
-            }
-            if (result.getType() == RayTraceResult.Type.BLOCK){
-                BlockPos hitPos = ((BlockRayTraceResult) result).getPos();
-                BlockState hitBlock = world.getBlockState(hitPos);
-                if (hitBlock.getBlock() == Blocks.MYCELIUM){
-                    world.setBlockState(hitPos, Blocks.GRASS_BLOCK.getDefaultState());
-                }
-            }
-            this.remove();
-        }
+        this.remove();
     }
 
-    @Override
-    public void tick() {
-        world.addParticle(ParticleTypes.FIREWORK, this.getPosX(), this.getPosY() - 0.3D, this.getPosZ(),
-                this.rand.nextGaussian() * 0.05D, -this.getMotion().y * 0.5D,
-                this.rand.nextGaussian() * 0.05D);
-        super.tick();
-    }
+
 }
--- src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/PurifyPowderItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/PurifyPowderItem.java
@@ -8,9 +8,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class PurifyPowderItem extends Item {
 
     public PurifyPowderItem(Properties properties) {
@@ -21,36 +18,11 @@ public class PurifyPowderItem extends Item {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack stack = playerIn.getHeldItem(handIn);
         if (!worldIn.isRemote){
-            if (!playerIn.isCrouching()) {
-                ArrayList<PurifyPowderEntity> purifyPowderEntity = new ArrayList<>();
-                Random roll = new Random();
-                purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
-                purifyPowderEntity.get(0).func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw,
-                        0f, 1.0f, 1f);
-                purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
-                purifyPowderEntity.get(1).func_234612_a_(playerIn, playerIn.rotationPitch + (float) ((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw + 10,
-                        0f, 1.0f, 1f);
-                purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
-                purifyPowderEntity.get(2).func_234612_a_(playerIn, playerIn.rotationPitch + (float) ((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw - 10,
-                        0f, 1.0f, 1f);
-                purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
-                purifyPowderEntity.get(3).func_234612_a_(playerIn, playerIn.rotationPitch + (float) ((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw + 20,
-                        0f, 1.0f, 1f);
-                purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
-                purifyPowderEntity.get(4).func_234612_a_(playerIn, playerIn.rotationPitch + (float) ((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw - 20,
-                        0f, 1.0f, 1f);
-                for (PurifyPowderEntity purifyPowder : purifyPowderEntity) {
-                    worldIn.addEntity(purifyPowder);
-                }
-            }
-            else {
-                PurifyPowderEntity purifyPowderEntity = new PurifyPowderEntity(worldIn, playerIn);
-                purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw,
-                        0f, 1.0f, 1f);
-                worldIn.addEntity(purifyPowderEntity);
-            }
+            PurifyPowderEntity purifyPowderEntity = new PurifyPowderEntity(worldIn, playerIn);
+            purifyPowderEntity.setItem(stack);
+            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0f, 1.5f, 1f);
+            worldIn.addEntity(purifyPowderEntity);
         }
-        stack.shrink(1);
         return ActionResult.resultSuccess(stack);
     }
 }
