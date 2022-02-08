be4d5eec84c8bcfb63a2387df4d99865bb987170
DUCVN <nguyenvietduc24@yahoo.com.vn>
2022-02-08 18:32:08 +0700

    finish purify powder

--- src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/PurifyPowderItem.java
+++ src/main/java/io/github/team_lodestar/transcendeum/item/mixcode/custom/PurifyPowderItem.java
@@ -1,12 +1,27 @@
 package io.github.team_lodestar.transcendeum.item.mixcode.custom;
 
 import io.github.team_lodestar.transcendeum.entity.mixcode.custom.PurifyPowderEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.renderer.entity.ShulkerBulletRenderer;
+import net.minecraft.client.renderer.entity.model.ShulkerBulletModel;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.projectile.EggEntity;
+import net.minecraft.entity.projectile.ShulkerBulletEntity;
+import net.minecraft.item.ArrowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class PurifyPowderItem extends Item {
 
@@ -18,10 +33,37 @@ public class PurifyPowderItem extends Item {
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack stack = playerIn.getHeldItem(handIn);
         if (!worldIn.isRemote){
-            PurifyPowderEntity purifyPowderEntity = new PurifyPowderEntity(worldIn, playerIn);
-            purifyPowderEntity.setItem(stack);
-            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0f, 1.5f, 1f);
-            worldIn.addEntity(purifyPowderEntity);
+            ArrayList<PurifyPowderEntity> purifyPowderEntity = new ArrayList<>();//PurifyPowderEntity(worldIn, playerIn);
+            Random roll = new Random();
+//            purifyPowderEntity.setItem(stack);
+            purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
+            purifyPowderEntity.get(0).func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw,
+                    0f, 1.0f, 1f);
+            purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
+            purifyPowderEntity.get(1).func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw + 10,
+                    0f, 1.0f, 1f);
+            purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
+            purifyPowderEntity.get(2).func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw - 10,
+                    0f, 1.0f, 1f);
+            purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
+            purifyPowderEntity.get(3).func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw + 20,
+                    0f, 1.0f, 1f);
+            purifyPowderEntity.add(new PurifyPowderEntity(worldIn, playerIn));
+            purifyPowderEntity.get(4).func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 3f), playerIn.rotationYaw - 20,
+                    0f, 1.0f, 1f);
+//            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw,
+//                    0f, 1.0f, 1f);
+//            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 0.05f), playerIn.rotationYaw + 5,
+//                    0f, 1.0f, 1f);
+//            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 0.05f), playerIn.rotationYaw - 5,
+//                    0f, 1.0f, 1f);
+//            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 0.05f), playerIn.rotationYaw + 10,
+//                    0f, 1.0f, 1f);
+//            purifyPowderEntity.func_234612_a_(playerIn, playerIn.rotationPitch + (float)((roll.nextGaussian() - 0.5) * 0.05f), playerIn.rotationYaw - 10,
+//                    0f, 1.0f, 1f);
+            for (PurifyPowderEntity purifyPowder : purifyPowderEntity) {
+                worldIn.addEntity(purifyPowder);
+            }
         }
         return ActionResult.resultSuccess(stack);
     }
