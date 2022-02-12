2fd29d3e31b9d41823b4c726fb35b9900c660cda
Ddx11 <Ddx11@DESKTOP-64A6TGP.home>
2022-02-12 15:48:33 +0100

    Added InfestedRock (Mob) (WIP) Retextured the viridian mires

---
+++ src/main/java/io/github/team_lodestar/transcendeum/procedures/InfectedRockEntityIsHurtProcedure.java
@@ -0,0 +1,42 @@
+package io.github.team_lodestar.transcendeum.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.IWorld;
+
+import java.util.Map;
+
+import io.github.team_lodestar.transcendeum.particle.ViridInfectionParticle;
+import io.github.team_lodestar.transcendeum.TheTranscendeumMod;
+
+public class InfectedRockEntityIsHurtProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				TheTranscendeumMod.LOGGER.warn("Failed to load dependency world for procedure InfectedRockEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				TheTranscendeumMod.LOGGER.warn("Failed to load dependency x for procedure InfectedRockEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				TheTranscendeumMod.LOGGER.warn("Failed to load dependency y for procedure InfectedRockEntityIsHurt!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				TheTranscendeumMod.LOGGER.warn("Failed to load dependency z for procedure InfectedRockEntityIsHurt!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		if (world instanceof ServerWorld) {
+			((ServerWorld) world).spawnParticle(ViridInfectionParticle.particle, x, y, z, (int) 5, 1, 1, 1, 0.2);
+		}
+	}
+}
