507cca598dd55c7d5ebf211f8e55f65b59525152
Toma400 <thomasso_calgori@o2.pl>
2022-03-20 13:47:33 +0100

    Typical "I couldn't import workspace" commit

--- src/main/java/io/github/team_lodestar/transcendeum/block/ThelyariumBlockBlock.java
+++ src/main/java/io/github/team_lodestar/transcendeum/block/ThelyariumBlockBlock.java
@@ -1,18 +1,33 @@
 
 package io.github.team_lodestar.transcendeum.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import java.util.List;
+import java.util.Collections;
+
+import io.github.team_lodestar.transcendeum.itemgroup.TranscendeumBlocksItemGroup;
+import io.github.team_lodestar.transcendeum.TheTranscendeumModElements;
 
 @TheTranscendeumModElements.ModElement.Tag
 public class ThelyariumBlockBlock extends TheTranscendeumModElements.ModElement {
-
 	@ObjectHolder("the_transcendeum:thelyarium_block")
 	public static final Block block = null;
 
 	public ThelyariumBlockBlock(TheTranscendeumModElements instance) {
 		super(instance, 420);
-
 	}
 
 	@Override
@@ -23,11 +38,9 @@ public class ThelyariumBlockBlock extends TheTranscendeumModElements.ModElement 
 	}
 
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
 			super(Block.Properties.create(Material.IRON).sound(SoundType.NETHERITE).hardnessAndResistance(2.2f, 25f).setLightLevel(s -> 0)
 					.harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().tickRandomly());
-
 			setRegistryName("thelyarium_block");
 		}
 
@@ -38,13 +51,10 @@ public class ThelyariumBlockBlock extends TheTranscendeumModElements.ModElement 
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
-
 	}
-
 }
