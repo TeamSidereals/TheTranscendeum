3e73e7022e63a21669ca4a64bad0718ce066525b
Sajevius <sajeviuscrend@gmail.com>
2022-06-12 10:21:40 +0800

    transcendent ray visual bugfix

--- src/main/java/io/github/team_lodestar/transcendeum/procedures/TranscendentRayOnEntityTickUpdateProcedure.java
+++ src/main/java/io/github/team_lodestar/transcendeum/procedures/TranscendentRayOnEntityTickUpdateProcedure.java
@@ -42,7 +42,7 @@ public class TranscendentRayOnEntityTickUpdateProcedure {
 		TranscendentRayBeamingProcedure
 				.executeProcedure(Stream
 						.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x),
-								new AbstractMap.SimpleEntry<>("y", 149), new AbstractMap.SimpleEntry<>("z", z))
+								new AbstractMap.SimpleEntry<>("y", 150), new AbstractMap.SimpleEntry<>("z", z))
 						.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 	}
 }
